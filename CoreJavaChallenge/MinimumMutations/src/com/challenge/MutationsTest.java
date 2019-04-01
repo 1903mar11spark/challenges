5d219fd1ea5e312abfb48771abbe2ae01ff71be4
Shane Flynn <shanef3@gmail.com>
2019-04-01 13:36:58 -0400

    Shane Silvia Final Push

--- CoreJavaChallenge/MinimumMutations/src/com/challenge/MutationsTest.java
+++ CoreJavaChallenge/MinimumMutations/src/com/challenge/MutationsTest.java
@@ -10,7 +10,7 @@ public class MutationsTest extends MutationsLogic {
 		System.out.println("Input a gene string.");
 		String first = user.nextLine();
 		if (first.length() > 8) {
-			System.out.println("Please input a string of 8 charcters.");
+			System.out.println("Please input a string of 8 characters.");
 		}
 		System.out.println("Input a gene string you want to mutate too.");	
 		String last = user.nextLine();
@@ -18,7 +18,7 @@ public class MutationsTest extends MutationsLogic {
 			System.out.println("Please input a string of 8 character.");
 		}
 		user.close();
-		System.out.println("THe Number of mutations is" + mutation(first, last));
+		System.out.println("THe Number of mutations is " + mutation(first, last));
 
 	}
 	
