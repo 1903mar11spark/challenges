734fb7c3dc7e459ec1b076ec599e827fa2a0f6fe
Birhan <birhan@USF-Gold-Wifi-226-109-71.laptops.usf.edu>
2019-04-29 12:01:01 -0400

    Step 1

---
+++ HibernateChallenge/src/main/java/com/challenge/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package com.challenge.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ConnectionUtil {
+	
+	private ConnectionUtil() {
+		super();
+	}
+	
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			
+			Configuration c = new Configuration();
+
+			c.setProperty("hibernate.connection.username", System.getenv("HIBERNATE_CHALLENGE_USER"));
+			c.setProperty("hibernate.connection.password", System.getenv("HIBERNATE_CHALLENGE_PASS"));
+			c.setProperty("hibernate.connection.url", System.getenv("HIBERNATE_CHALLENGE_URL"));
+			c.configure();
+			
+			sessionFactory = c.buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+
+}
